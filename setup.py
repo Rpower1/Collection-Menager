0c968125968ac6e72c1103217e163dec43b41c20
Rpower1 <148472541+Rpower1@users.noreply.github.com>
2025-07-12 18:49:00 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='CollectionMenager',
+    version='0.1',
+    description='Collection Menager App',
+    author='Robin',
+    packages=[],
+    include_package_data=True,
+    install_requires=[],
+)
